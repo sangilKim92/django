18c00eff0af20b1e6e29ad49a012ddb477c82ea6
sangilKim92 <ksl13579@hanmail.net>
2020-07-17 11:05:52 +0900

    약간의 수정

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('add/', views.BookmarkCreateView.as_view(), name="add",),
     path('change/', views.BookmarkChangeLV.as_view(), name="change",),
-    path('<int:pk>/update/', views.BookmarkUpdateView.as_view(), name="update",),
-    path('<int:pk>/delete/', views.BookmarkDeleteView.as_view(), name="delete",),
+    path('update/<int:pk>', views.BookmarkUpdateView.as_view(), name="update",),
+    path('delete/<int:pk>', views.BookmarkDeleteView.as_view(), name="delete",),
 
 ]
